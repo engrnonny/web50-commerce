19444418b87b654236814bf0a3694b5e754eca70
engrnonny <engr.nonny@gmail.com>
2021-09-30 11:26:18 +0100

    Re-copied files

---
+++ auctions/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Auction(models.Model):
+    title = models.CharField(max_length=25)
+    category = models.CharField(max_length=255)
+    description = models.TextField()
+    lister = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+#image
+
+class Bid(models.Model):
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    starting_bid = models.IntegerField(default=0)  
+    current_price = models.IntegerField(default=0)
+    
+class Comment(models.Model):
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+class Wishlist(models.Model):
+    User = models.OneToOneField(User, on_delete=models.CASCADE)
+    auction = models.ManyToManyField(Auction)
